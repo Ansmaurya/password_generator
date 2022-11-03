32f444c30222a81f021c01bdaecc1691559960ff
rdmaurya <117154904+rdmaurya@users.noreply.github.com>
2022-11-03 23:51:33 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -9,8 +9,7 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
 password_list=[]
 for char in range(0, nr_letters):
   password_list.append(random.choice(letters))
